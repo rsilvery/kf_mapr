0e0da83ddb09e8ba6285165894167fbc650ac599
Rachel <rsilver@mapr.com>
2018-05-14 11:45:21 -0400

    steps to deploy

---
+++ README.md
@@ -0,0 +1,25 @@
+Steps:
+
+1. Create namespace and enable volume plugin:
+ "kubectl create -f mapr_config.yaml"
+2. Install KSonnet:
+wget https://github.com/ksonnet/ksonnet/releases/download/v0.10.2/ks_0.10.2_linux_amd64.tar.gz
+tar -xzvf ks_0.10.2_linux_amd64.tar.gz 
+3. Run create kubeflow app: 
+
+cd ks_0.10.2_linux_amd64
+./ks init kubeflow-demo
+cd kubeflow-demo/
+../ks env set default --namespace demo-kf
+../ks registry add kubeflow github.com/kubeflow/kubeflow/tree/v0.1.3/kubeflow
+
+# Install Kubeflow components
+../ks pkg install kubeflow/core@v0.1.3
+../ks pkg install kubeflow/tf-serving@v0.1.3
+../ks pkg install kubeflow/tf-job@v0.1.3
+
+# Create templates for core components
+../ks generate kubeflow-core kubeflow-core
+
+# Deploy Kubeflow
+../ks apply default -c kubeflow-core
--- kubeflow_init.sh
+++ kubeflow_init.sh
@@ -1,21 +1,18 @@
-# Set Env Vars for kubeflow deployment
-NAMESPACE=demo-kf
-VERSION=v0.1.3
 
 # Initialize a ksonnet app. Set the namespace for it's default environment.
 APP_NAME=my-kubeflow
-ks init ${APP_NAME}
+ks_0.10.2_linux_amd64/ks init ${APP_NAME}
 cd ${APP_NAME}
-ks env set default --namespace ${NAMESPACE}
+ks_0.10.2_linux_amd64/ks env set default --namespace demo-kf
 
 # Install Kubeflow components
-ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${VERSION}/kubeflow
-ks pkg install kubeflow/core@${VERSION}
-ks pkg install kubeflow/tf-serving@${VERSION}
-ks pkg install kubeflow/tf-job@${VERSION}
+ks_0.10.2_linux_amd64/ks registry add kubeflow github.com/kubeflow/kubeflow/tree/v0.1.3/kubeflow
+ks_0.10.2_linux_amd64/ks pkg install kubeflow/core@v0.1.3
+ks_0.10.2_linux_amd64/ks pkg install kubeflow/tf-serving@v0.1.3
+ks_0.10.2_linux_amd64/ks pkg install kubeflow/tf-job@v0.1.3
 
 # Create templates for core components
-ks generate kubeflow-core kubeflow-core
+ks_0.10.2_linux_amd64/ks generate kubeflow-core kubeflow-core
 
 # Deploy Kubeflow
-ks apply default -c kubeflow-core
+ks_0.10.2_linux_amd64/ks apply default -c kubeflow-core
