fca818448f0da325dd3ae9aeaf3e042532bc19e0
Rachel <rsilver@mapr.com>
2018-05-14 11:15:11 -0400

    init script for kubeflow

---
+++ kubeflow_init.sh
@@ -0,0 +1,21 @@
+# Set Env Vars for kubeflow deployment
+NAMESPACE=demo-kf
+VERSION=v0.1.3
+
+# Initialize a ksonnet app. Set the namespace for it's default environment.
+APP_NAME=my-kubeflow
+ks init ${APP_NAME}
+cd ${APP_NAME}
+ks env set default --namespace ${NAMESPACE}
+
+# Install Kubeflow components
+ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${VERSION}/kubeflow
+ks pkg install kubeflow/core@${VERSION}
+ks pkg install kubeflow/tf-serving@${VERSION}
+ks pkg install kubeflow/tf-job@${VERSION}
+
+# Create templates for core components
+ks generate kubeflow-core kubeflow-core
+
+# Deploy Kubeflow
+ks apply default -c kubeflow-core
